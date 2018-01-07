a5412e09962f28a51bbe59abb5143d0ceec2100d
willchen783 <willchening@gmail.com>
2018-01-07 12:41:01 +0800

    重構controller 和view

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,8 @@
 class TodosController < ApplicationController
+  before_action :set_todo, :only => [:show, :edit, :update, :destroy, :check]
+  # 透過 before_action 來篩選指定動作，我們稱之為 過濾器 (Filter)。除了 before_action 之外，Rails 也提供 around_action 和 after_action 來讓我們來定義進入 action 之中或之後來執行特定的方法。其中 before_action 最為常用。
+  # 
+  # before_action 也可以是回呼方法 (Callback Method) 的一種。除了針對 Controller 之外，也有針對 Model (也就是 Active Record) 的 Callback Method. 我們會在後來的章節，跟大家介紹。關於 Active Record 的 Callback Method，你可以參考官方指南。
   def index
     @todos = Todo.all
     
@@ -18,18 +22,18 @@ class TodosController < ApplicationController
     end
   end
 
-  def show
-    @todo=Todo.find(params[:id])
+  # def show
+  #   @todo=Todo.find(params[:id])
     
-  end
+  # end
 
-  def edit
-    @todo=Todo.find(params[:id])
+  # def edit
+  #   @todo=Todo.find(params[:id])
     
-  end
+  # end
 
   def update
-    @todo=Todo.find(params[:id])
+    # @todo=Todo.find(params[:id])
     if @todo.update_attributes(todo_params)
 
       redirect_to todo_url(@todo)
@@ -39,7 +43,7 @@ class TodosController < ApplicationController
   end
 
   def destroy
-    @todo=Todo.find(params[:id])
+    # @todo=Todo.find(params[:id])
     @todo.destroy
 
     redirect_to todos_url
@@ -47,12 +51,15 @@ class TodosController < ApplicationController
   end
 
   def check
-    @todo=Todo.find(params[:id])
+    # @todo=Todo.find(params[:id])
     @todo.update(checkbox: !(@todo.checkbox))
     
   end
 private
-  
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+
   def todo_params
     params.require(:todo).permit(:title,:due_date,:description)
   
